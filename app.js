fc4a3bb0f3315a205b03a8f66a30beeb66c36b37
s1dexdev <s1dexdev@gmail.com>
2021-06-27 12:14:32 +0300

    added structure and db(mongoose)

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-const express = require("express");
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const HttpCode = require('./helpers/constants');
+
 const app = express();
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.listen(3030, () => {
-  console.log("Example app listening on port 3000!");
+app.use((req, res) => {
+  res
+    .status(HttpCode.NOT_FOUND)
+    .json({ status: 'error', code: HttpCode.NOT_FOUND, message: 'Not found' });
 });
+
+app.use((err, req, res, next) => {
+  const code = err.status || HttpCode.INTERNAL_SERVER_ERROR;
+  const statusError = err.status ? 'error' : 'fail';
+
+  res.status(code).json({ status: statusError, code, message: err.message });
+});
+
+module.exports = app;
