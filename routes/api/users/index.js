4db285fe98a58aa8c1cb57a178e650d821027228
s1dexdev <s1dexdev@gmail.com>
2021-06-27 18:03:14 +0300

    logout

--- routes/api/users/index.js
+++ routes/api/users/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../../../controllers/users');
+const guard = require('../../../helpers/guard');
 const { validateCreateUser } = require('./validation_schema');
 
 router.post('/signup', validateCreateUser, ctrl.signup);
 router.post('/login', ctrl.login);
+router.post('/logout', guard, ctrl.logout);
 
 module.exports = router;
