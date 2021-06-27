ea044f3c4861ce5ce56423fa75cdc71985f26a9f
s1dexdev <s1dexdev@gmail.com>
2021-06-27 15:38:04 +0300

    added authentication

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 const Users = require('../model/users');
 const HttpCode = require('../helpers/constants');
 
@@ -30,6 +33,41 @@ const signup = async (req, res, next) => {
   }
 };
 
+const login = async (req, res, next) => {
+  try {
+    const user = await Users.findByEmail(req.body.email);
+    const isValidPassword = await user?.validPassword(req.body.password);
+
+    if (!user || !isValidPassword) {
+      return res.status(HttpCode.UNAUTHORIZED).json({
+        status: 'error',
+        code: HttpCode.UNAUTHORIZED,
+        message: 'Email or password is wrong',
+      });
+    }
+
+    const payload = { id: user.id };
+    const token = jwt.sign(payload, JWT_SECRET_KEY, { expiresIn: '2h' });
+    const { email } = user;
+
+    await Users.updateToken(user.id, token);
+
+    return res.status(HttpCode.OK).json({
+      status: 'success',
+      code: HttpCode.OK,
+      data: {
+        token,
+        user: {
+          email,
+        },
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   signup,
+  login,
 };
