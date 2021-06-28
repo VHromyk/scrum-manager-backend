dead53f0d38c2c0de0afa11a5ca7a3646ee4ea36
s1dexdev <s1dexdev@gmail.com>
2021-06-28 14:27:23 +0300

    sprint

--- routes/api/projects/validation_schema.js
+++ routes/api/projects/validation_schema.js
@@ -1,8 +1,14 @@
 const Joi = require('joi');
 
 const schemaCreateProject = Joi.object({
-  name: Joi.string().min(3).max(20).required(),
+  name: Joi.string().min(4).max(20).required(),
   description: Joi.string().min(10).max(100),
+});
+
+const schemaCreateSprint = Joi.object({
+  name: Joi.string().min(4).max(20).required(),
+  startDate: Joi.string().min(3).max(30).required(),
+  duration: Joi.number().integer().required(),
 });
 
 const validate = async (schema, body, next) => {
@@ -16,4 +22,8 @@ const validate = async (schema, body, next) => {
 
 module.exports.validateCreateProject = (req, _res, next) => {
   return validate(schemaCreateProject, req.body, next);
+};
+
+module.exports.validateCreateSprint = (req, _res, next) => {
+  return validate(schemaCreateSprint, req.body, next);
 };
